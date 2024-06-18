d51c637105bf0ca31d74239b4e190786511e652c
lauraamelina <lauraamelina@gmail.com>
2024-06-18 14:31:34 -0300

    feat: add UserRoute in index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import UserRoute from './routes/usuarios.api.route.js';
 
 const app = express();
 app.use(bodyParser.json())
 app.set('port', 8080)
 app.use(cors())
+
+app.use('/usuarios/', UserRoute)
 
 app.get('/', (req, res) => {
     res.send({
