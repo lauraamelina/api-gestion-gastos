371caac806010f44833cd24a534b0828898ff850
lauraamelina <lauraamelina@gmail.com>
2024-06-19 19:39:44 -0300

    feat: add function getSubCategoriaByCategoriaId in controller subcategoria

--- controllers/subcategorias.api.controller.js
+++ controllers/subcategorias.api.controller.js
@@ -19,7 +19,18 @@ const getSubCategoriesById = async (req, res) => {
     }
 }
 
+const getSubCategoriaByCategoriaId = async (req, res) => {
+    const id = req.params.id
+    try {
+        const response = await service.getSubCategoriaByCategoriaId(id)
+        return res.status(200).json({ status: true, data: response })
+    } catch (err) {
+        return res.status(500).json({ status: false, error: err.message })
+    }
+}
+
 export {
     getAllSubCategories,
-    getSubCategoriesById
+    getSubCategoriesById,
+    getSubCategoriaByCategoriaId
 }
