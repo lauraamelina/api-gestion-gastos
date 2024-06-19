1360ca8560c2982da4c2e9f566a88bfe17dacf15
lauraamelina <lauraamelina@gmail.com>
2024-06-19 19:23:09 -0300

    feat: add function getCategoriesById in categoria.controller

--- controllers/categorias.api.controller.js
+++ controllers/categorias.api.controller.js
@@ -9,6 +9,17 @@ const getAllCategories = async (req, res) => {
     }
 }
 
+const getCategoriesById = async (req, res) => {
+    const id = req.params.id
+    try {
+        const response = await service.getCategoriesById(id)
+        return res.status(200).json({ status: true, data: response })
+    } catch (err) {
+        return res.status(500).json({ status: false, error: err.message })
+    }
+}
+
 export {
-    getAllCategories
+    getAllCategories,
+    getCategoriesById
 }
